00516bbd0dfab89321792e30d7b0cb7cf42947b3
Naimur Reza <naimur.rezaa@gmail.com>
2023-11-28 18:34:15 +0600

    Add global try catch async

--- src/app/modules/student.controller.ts
+++ src/app/modules/student.controller.ts
@@ -1,61 +1,40 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
 import { StudentServices } from "./student.service";
 import { studentValidationSchema } from "./student.validation";
 import sendResponse from "../utils/sendResponse";
 import httpStatus from "http-status";
 
-const createStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const student = req.body;
-
-    const zodParseData = studentValidationSchema.parse(student);
-
-    await StudentServices.createStudentInDb(zodParseData);
-
-    sendResponse(res, {
-      success: true,
-      message: "Student info inserted successfully",
-      data: res,
-      statusCode: httpStatus.OK,
-    });
-  } catch (error) {
-    console.error("Error in createStudent:", error);
-    next(error);
-  }
+const catchAsync = (fn: RequestHandler) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch(err => next(err));
+  };
 };
 
-const getAllStudents = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const data = await StudentServices.getStudents();
-    return res.status(200).send(data);
-  } catch (error) {
-    console.error("Error in getAllStudents:", error);
-    next(error);
-  }
-};
+const createStudent: RequestHandler = catchAsync(async (req, res, next) => {
+  const student = req.body;
 
-const getSingleStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const id = req.params.id;
-    const data = await StudentServices.getSingleStudentFromDB(id);
-    return res.status(200).send(data);
-  } catch (error) {
-    console.error("Error in getSingleStudent:", error);
-    next(error);
-  }
-};
+  const zodParseData = studentValidationSchema.parse(student);
+
+  await StudentServices.createStudentInDb(zodParseData);
+
+  sendResponse(res, {
+    success: true,
+    message: "Student info inserted successfully",
+    data: res,
+    statusCode: httpStatus.OK,
+  });
+});
+
+const getAllStudents: RequestHandler = catchAsync(async (req, res, next) => {
+  const data = await StudentServices.getStudents();
+  return res.status(200).send(data);
+});
+
+const getSingleStudent: RequestHandler = catchAsync(async (req, res, next) => {
+  const id = req.params.id;
+  const data = await StudentServices.getSingleStudentFromDB(id);
+  return res.status(200).send(data);
+});
 
 export const StudentController = {
   createStudent,
