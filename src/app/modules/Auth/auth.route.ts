736bd1c793e3222cd3a4193e448e09bae8fb8072
Naimur Reza <naimur.rezaa@gmail.com>
2023-12-20 18:11:51 +0600

    Password change added

--- src/app/modules/Auth/auth.route.ts
+++ src/app/modules/Auth/auth.route.ts
@@ -4,6 +4,8 @@ import validateRequest from '../../middlewares/validateRequest';
 // import { USER_ROLE } from './../user/user.constant';
 import { AuthControllers } from './auth.controller';
 import { AuthValidation } from './auth.validation';
+import { USER_ROLE } from '../user/user.constant';
+import auth from '../../middlewares/auth';
 
 const router = express.Router();
 
@@ -15,27 +17,27 @@ router.post(
 
 router.post(
   '/change-password',
-  // auth(USER_ROLE.admin, USER_ROLE.faculty, USER_ROLE.student),
+  auth(USER_ROLE.admin, USER_ROLE.faculty, USER_ROLE.student),
   validateRequest(AuthValidation.changePasswordValidationSchema),
   AuthControllers.changePassword,
 );
 
-router.post(
-  '/refresh-token',
-  validateRequest(AuthValidation.refreshTokenValidationSchema),
-  AuthControllers.refreshToken,
-);
+// router.post(
+//   '/refresh-token',
+//   validateRequest(AuthValidation.refreshTokenValidationSchema),
+//   AuthControllers.refreshToken,
+// );
 
-router.post(
-  '/forget-password',
-  validateRequest(AuthValidation.forgetPasswordValidationSchema),
-  AuthControllers.forgetPassword,
-);
+// router.post(
+//   '/forget-password',
+//   validateRequest(AuthValidation.forgetPasswordValidationSchema),
+//   AuthControllers.forgetPassword,
+// );
 
-router.post(
-  '/reset-password',
-  validateRequest(AuthValidation.forgetPasswordValidationSchema),
-  AuthControllers.resetPassword,
-);
+// router.post(
+//   '/reset-password',
+//   validateRequest(AuthValidation.forgetPasswordValidationSchema),
+//   AuthControllers.resetPassword,
+// );
 
 export const AuthRoutes = router;
