0ef3e0122ceda4fd6ca86ca2130f76c0e10e1617
Naimur Reza <naimur.rezaa@gmail.com>
2024-01-19 18:22:55 +0600

    Solved several bugs

--- src/app/modules/offeredCourse/offeredCourse.route.ts
+++ src/app/modules/offeredCourse/offeredCourse.route.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
-import { OfferedCourseControllers } from './OfferedCourse.controller';
+
 import { OfferedCourseValidations } from './offeredCourse.validation';
+import { OfferedCourseControllers } from './offeredCourse.controller';
 
 const router = express.Router();
 
