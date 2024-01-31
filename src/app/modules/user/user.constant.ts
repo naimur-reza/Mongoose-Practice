9ed40e0589d5b017f145d9eeb0db4f4b5aa7fbdf
Naimur Reza <naimur.rezaa@gmail.com>
2024-01-31 19:11:46 +0600

    Added superAdmin

--- src/app/modules/user/user.constant.ts
+++ src/app/modules/user/user.constant.ts
@@ -2,6 +2,7 @@ export const USER_ROLE = {
   admin: 'admin',
   student: 'student',
   faculty: 'faculty',
+  superAdmin: 'super-admin',
 } as const;
 
 export const UserStatus = {
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose';
 import app from './app';
 import config from './app/config';
+import seedSuperAdmin from './app/DB';
 
 async function main() {
   try {
     await mongoose.connect(config.database_url_live as string);
 
+    seedSuperAdmin();
     app.listen(config.port, () => {
       console.log('Database connected!');
       console.log(`app is listening on port ${config.port}`);
