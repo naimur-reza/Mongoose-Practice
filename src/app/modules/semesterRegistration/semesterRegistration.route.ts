56fa3c4ecee7e6ecc9766eb6718b79e37237d662
Naimur Reza <naimur.rezaa@gmail.com>
2024-02-02 19:28:31 +0600

    Add auth guard semester reg

--- src/app/modules/semesterRegistration/semesterRegistration.route.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.route.ts
@@ -5,32 +5,49 @@ import {
   updateSemesterRegistrationValidationSchema,
 } from './semesterRegistration.validation';
 import validateRequest from '../../middlewares/validateRequest';
+import { USER_ROLE } from '../user/user.constant';
+import auth from '../../middlewares/auth';
 
 const router = Router();
 
 router.post(
   '/create-semester-registration',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(createSemesterRegistrationValidationSchema),
   SemesterRegistrationControllers.createSemesterRegistrationIntoDB,
 );
 
 router.get(
   '/',
+  auth(
+    USER_ROLE.superAdmin,
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
   SemesterRegistrationControllers.getAllSemesterRegistrationIntoDB,
 );
 router.get(
   '/:id',
+  auth(
+    USER_ROLE.superAdmin,
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
   SemesterRegistrationControllers.getSingleSemesterRegistrationIntoDB,
 );
 
 router.patch(
   '/:id',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(updateSemesterRegistrationValidationSchema),
   SemesterRegistrationControllers.updateSemesterRegistrationIntoDB,
 );
 
 router.delete(
   '/:id',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   SemesterRegistrationControllers.deleteSemesterRegistrationFromDB,
 );
 
